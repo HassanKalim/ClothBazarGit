bc075f81d5e1a203d12a4899647c31acd7632290
cybill.netsol@gmail.com <cybill.netsol@gmail.com>
2019-01-11 17:25:05 +0500

    break time

---
+++ ClothBazar.Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+   public class Category : BaseEntity
+    {
+        // BaseEnity Inheritance kar raha apni porperty idr 
+
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ ClothBazar.Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+   public class Product : BaseEntity
+    {
+        // BaseEnity Inheritance kar raha apni porperty idr 
+
+       
+        public Decimal Price { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
